2e8f8d8ab7544066f454515018b83380979966f8
kenzop <43009804+kenzop@users.noreply.github.com>
2018-10-03 13:19:48 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,2 +1,6 @@
-# hangman.py 
-    
+secret_word = ""
+def get_secret_word(secret_word):
+  import random
+  word_bank = ['apple', 'banana', 'orange', 'pear', 'mango', 'grapes']
+  secret_word = random.choice(word_bank)
+  return secret_word
