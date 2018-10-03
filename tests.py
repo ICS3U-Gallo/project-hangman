f775b5fd2c861cd62cf7c0a7762793c2652a6861
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2018-10-03 09:22:24 -0400

    Add tests.py (#47)
    * Update README.md
    
    * Update README.md
    
    * Create tests.py
    
    * Update tests.py

---
+++ tests.py
@@ -0,0 +1,21 @@
+def get_secret_word():
+    words = {get_secret_word() for _ in range(100)}
+    assert len(words) >= 5, "Should contain a list of at least 5 words."
+    
+    # do the following:
+    assert what secret word returns is a string, "Should return a string"
+
+
+def test_word_is_solved():
+    assert word_is_solved("hello", "eh") == False, "Should not be solved guesses: 'eh' for word: 'hello'."
+    assert word_is_solved("world", "dorlw") == True, "Should be solved"
+
+    
+def test_display_letters():
+    assert display_letters("hello", "h") == "h _ _ _ _", "Should reveal only first letter"
+
+    
+# Test display_platform(wrong_guesses)
+# display_platform("a") -> display with head
+
+
