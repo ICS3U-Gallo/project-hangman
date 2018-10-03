4b7f0b054f1c00aa8ef1b008ce3b93b21b81e577
Therealweardbop(RY) <38886475+therealweardbop@users.noreply.github.com>
2018-10-03 09:23:56 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,4 +2,5 @@ import random
 secretWordList = ['hello', 'turtle', 'potato', 'water', 'turnip', 'magic', 'cow', 'champion', 'return', 'computer']
 number = random.randint(0,len(secretWordList)-1)
 secretWord = secretWordList[number]
+print(secretWord)
 
