8808dfbbff21198cdad8c544c338c7d40df36bb4
Patrick000000 <43682578+Patrick000000@users.noreply.github.com>
2018-10-03 12:24:49 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,2 +1,177 @@
 # hangman.py 
     
+def main():
+    secret_word = get_secret_word()
+    correct_guesses = ""
+    wrong_guesses = ""
+
+    while (not word_is_solved(secret_word, correct_guesses) and
+            not player_is_dead(wrong_guesses)):
+        display_platform(wrong_guesses)
+        display_letters(secret_word, correct_guesses)
+        guess = get_guess()
+
+        # HANDLE GUESS
+        # if guess is in the word, the add it to the correct guesses
+        # otherwise, add it to the incorrect guesses
+
+    if word_solved(secret_word, correct_guesses):
+        show_win_screen()
+    else:
+        show_lose_screen()
+
+
+def get_secret_word():
+    """Will return a single word from a list of words.
+    
+    Returns:
+        Single word from a list
+    """
+    import random
+    word_bank = [
+        "Dog", "Matcha", "Brain", "Brother", "Player", "Cerebrum",
+        "Trachea", "Fiblia", "Construct", "Construction", "Cell",
+        "Mitochondria", "Power", "Calcium"
+    ]
+    return random.choice(word_bank)
+
+
+
+def word_is_solved(word, guesses):
+    """Will check to see if a word has been completely solved.
+    
+    Args:
+        word (str): The secret word
+        guesses (str): The string containing all correct guesses
+        
+    Returns:
+        bool: True if word is solved, False if word is not.
+    """
+    pass
+
+
+def player_is_dead(wrong_guesses):
+    """Determines if the player is dead (too many guesses).
+    
+    Args:
+        bad_guesses (str): A string containing all of the wrong guesses
+        
+    Returns
+        bool: True if dead, False otherwise
+    """
+    pass
+
+
+def display_platform(wrong_guesses):
+    """Displays (prints) hanging platform based on how many incorrect guesses.
+    
+    Args:
+        wrong_guesses (str): A string containing all wrong guesses.
+    """
+
+    def display_platform(wrong_guesses):
+    if len(wrong_guesses) == 0:
+        print('''
+ _____
+ |
+ |
+ |
+_|__
+''')
+    elif len(wrong_guesses) == 1:
+        print('''
+ _____
+ |  Ó
+ |
+ |
+_|__
+''')
+    elif len(wrong_guesses) == 2:
+        print('''
+ _____
+ |  Ó
+ |  |
+ |
+_|__
+''')
+    elif len(wrong_guesses) == 3:
+        print('''
+ _____
+ |  Ó
+ | /|
+ |
+_|__
+''')
+    elif len(wrong_guesses) == 4:
+        print('''
+ _____
+ |  Ó
+ | /|\ 
+ |
+_|__
+''')
+    elif len(wrong_guesses) == 5:
+        print('''
+ _____
+ |  Ó
+ | /|\ 
+ | /
+_|__
+''')
+    else:
+        print('''
+ _____
+ |  Ó
+ | /|\ 
+ | / \ 
+_|__
+''')
+        
+
+def display_letters(word, guesses):
+    """Displays (prints) the letter-board,
+    underscores (_) for letters not yet guessed.
+    
+    Args:
+        word (str): The secret word
+        guesses (str): A string of correct guesses
+    """
+    pass
+
+
+def get_guess():
+      validity = False
+  while validity == False:
+    letter = input('Enter a letter:')
+    if letter in "qwertyuiopasdfghjklzxcvbnm" and len(letter) == 1:
+        validity = True
+    else:
+      print('Character is invalid. ',end='')
+    
+    if validity == True:
+      return letter
+    pass
+
+
+def show_win_screen():
+    """Will display ASCII art for a win screen"""
+    print("_____.___.                      .__         ._.         ___    ")
+    print("\__  |   | ____  __ __  __  _  _|__| ____   | |     /\  \  \   ")
+    print(" /   |   |/  _ \|  |  \ \ \/ \/ /  |/    \  | |     \/   \  \  ")
+    print(" \____   (  <_> )  |  /  \     /|  |   |  \  \|     /\    )  ) ")
+    print(" / ______|\____/|____/    \/\_/ |__|___|  /  __     \/   /  /  ")
+    print(" \/                                     \/   \/         /__/   ")
+
+
+def show_lose_screen():
+    """Will display ASCII art for a lose screen"""
+    print("_____.___.              .__                        ._.            ___ ")
+    print("\__  |   | ____  __ __  |  |   ____  ______ ____   | |     /\    /  / ")
+    print(" /   |   |/  _ \|  |  \ |  |  /  _ \/  ___// __ \  | |     \/   /  /  ")
+    print(" \____   (  <_> )  |  / |  |_(  <_> )___ \\  ___/   \|      /\  (  (   ")
+    print(" / ______|\____/|____/  |____/\____/____  >\___  >  __     \/   \  \  ")
+    print(" \/                                     \/     \/   \/           \__\ ")
+
+
+if __name__ == "__main__":
+    main()
