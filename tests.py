37859cc10d71ba5c8a3242e59ccdc819a5bb2c4b
Maggieeli <43610449+Maggieeli@users.noreply.github.com>
2018-10-03 09:14:26 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import random
 secretWordList = ['hello', 'turtle', 'potato', 'water', 'turnip', 'magic', 'cow', 'champion', 'return', 'computer']
-number = random.randint(0,len(secretWordList))
+number = random.randint(0,len(secretWordList)-1)
 secretWord = secretWordList[number]
+
