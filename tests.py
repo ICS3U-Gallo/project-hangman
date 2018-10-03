8faa8b7dae9d3daa34ab6538005a54636a7df38a
eric21ji <43616400+eric21ji@users.noreply.github.com>
2018-10-03 14:23:03 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -15,6 +15,15 @@ def test_word_is_solved():
     
 def test_display_letters():
     assert display_letters("hello", "h") == "h _ _ _ _", "Should reveal only first letter"
+    assert display_letters("hello", "l") == "_ _ l l _", "Should display both characters"
+    assert display_letters("aaa", "bca") == "a a a", "Should display all characters"
+    assert display_letters("world", "yqae") == "_ _ _ _ _", "Should display no letters"
+    assert display_letters(" ", "guess") == " ", "Should display the space"
+    assert display_letters("hello ", "gueso") == "_ e _ _ o  ", "should display e and o"
+    assert display_letters("ppap", "pp") == "p p _ p", "Should print three p's"
+    assert display_letters("bye bye", " ") == "_ _ _   _ _ _", "Should print proper blanks"
+    assert display_letters("", "aswe") == "", "Should not print anything"
+    assert display_letters("he llo", "a a a h") == "h _   _ _ _", "Should print the space and the h"
 
     
 # Test display_platform(wrong_guesses)
