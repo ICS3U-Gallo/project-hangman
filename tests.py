af30495c8da3549a0bf3e085ba507a895b12e968
joshchwang <43012087+joshchwang@users.noreply.github.com>
2018-10-04 14:29:13 -0400

    Update tests.py  display_letters

--- tests.py
+++ tests.py
@@ -19,7 +19,18 @@ def test_word_is_solved():
     
 def test_display_letters():
     assert display_letters("hello", "h") == "h _ _ _ _", "Should reveal only first letter"
-
+    assert display_letters("hello", "hello") == "h e l l o", "Should reveal entire word"
+    assert display_letters("hello", "wptrk") == "_ _ _ _ _", "Should reveal nothing of the word"
+    assert display_letters("hello john", "hlljoenh") == "h e l l o j o h n", "Should reveal whole phrase"
+    assert display_letters("hello john", "eljh") == "_ e l l _ j _ h _", "Should reveal most characters"
+    assert display_letters("wow", "w") == "w _ w", "Should only reveal w's"
+    assert display_letters("yesterday", "ystd") == "y _ s t _ _ d _ y", "Should reveal most characters"
+    assert display_letters("wow thats cool" "") == "_ _ _ _ _ _ _ _ _ _ _ _", "Should reveal nothing but underscores"
+    assert display_letters("wow thats cool", "woclthas") == "w o w t h a t s c o o l", "Should reveal entire phrase"
+    assert display_letters("josh is the best", "osh") == "_ o s h _ s _ h _ _ _s_", "Should reveal most characters"
+    assert display_letters("wowie" "abcdfghjklmnpqrstuvxyz") == "_ _ _ _ _", "Should only reveal underscores"
+    assert display_letters("handy", "ad") == "_ a _ d _", "Should reveal ad"
+    assert display_letters("tiredness", "tnsd") == "t _ _ _ d n _ s s", "Should reveal some characters"
     
 # Test display_platform(wrong_guesses)
 # display_platform("a") -> display with head
