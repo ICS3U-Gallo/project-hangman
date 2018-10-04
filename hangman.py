cf1d1a796693972d1d9d9b73f42f04706feddbf9
eric21ji <43616400+eric21ji@users.noreply.github.com>
2018-10-04 13:03:38 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -185,7 +185,7 @@ def display_platform(wrong_guesses):
         print(display_platform6)
 
 
-def display_letters(word, guesses):
+def get_blanked_word(word, guesses):
     """Displays (prints) the letter-board,
     underscores (_) for letters not yet guessed.
     Args:
@@ -196,16 +196,17 @@ def display_letters(word, guesses):
     guesses = list(set(guesses))
     word_list = list(word)
 
-    for k in range(0, len(word)):
+    for k in range(len(word)):
         if(letters[k] != " "):
             letters[k] = "_"
 
-    for i in range(0, len(guesses)):
-        for k in range(0, word.count(guesses[i])):
-            if(guesses[i] in word):
-                ind = word_list.index(guesses[i])
-                letters[ind] = guesses[i]
-                word_list[ind] = "_"
+    for guess_letter in guesses:
+      num_of_guess_letters = word.count(guess_letter)
+      for j in range(num_of_guess_letters):
+          if(guess_letter in word):
+              ind = word_list.index(guess_letter)
+              letters[ind] = guess_letter
+              word_list[ind] = "_"
 
     return " ".join(letters)
 
