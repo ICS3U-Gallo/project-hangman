d504d34d2c842673e61a7be2cb132a667116f0f1
Maggieeli <43610449+Maggieeli@users.noreply.github.com>
2018-10-04 09:26:39 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
-import random
-secretWordList = ['hello', 'turtle', 'potato', 'water', 'turnip', 'magic', 'cow', 'champion', 'return', 'computer']
-number = random.randint(0,len(secretWordList)-1)
-secretWord = secretWordList[number]
+def get_secret_word():
+    import random
+    secretWordList = ['hello', 'turtle', 'potato', 'water', 'turnip', 'magic', 'cow', 'champion', 'return', 'computer']
+    number = random.randint(0,len(secretWordList)-1)
+    secretWord = secretWordList[number]
+    return secretWord
 
 
 
