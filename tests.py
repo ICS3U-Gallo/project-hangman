56b2a35df797f3a989ce284ebc6814eaa3af5423
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2018-10-05 14:30:36 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from hangman import *
 
+
 def test_get_secret_word():
     words = {get_secret_word() for _ in range(100)}
     assert len(words) >= 5, "Should contain a list of at least 5 words."
@@ -9,8 +10,12 @@ def test_get_secret_word():
 
 
 def test_word_is_solved():
-    assert word_is_solved("hello", "eh") == False, "Should not be solved guesses: 'eh' for word: 'hello'."
-    assert word_is_solved("world", "dorlw") == True, "Should be solved"
+    assert word_is_solved("hello", "eh") == False,"Should not be solved guesses: 'eh' for word: 'hello'."
+    assert word_is_solved("world", "dorlw") == True,"Should be solved"
+    assert word_is_solved("paper", "ptaru") == False,"Should not be solved guesses: 'ptaru' for word: 'paper'."
+    assert word_is_solved("table", "aeotbhl") == True,"Should be solved"
+    assert word_is_solved("hello world", "eawldhotr") == True,"Should be solved"
+    assert word_is_solved("hello world", "eawdhotr") == False,"Should not be solved guesses: 'eawl dhotr' for word:'hello world'."
 
     
 def test_display_letters():
@@ -19,5 +24,3 @@ def test_display_letters():
     
 # Test display_platform(wrong_guesses)
 # display_platform("a") -> display with head
-
-
