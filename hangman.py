4296df88eabd55a2da31447c1afa98237ccc2ea7
340757871 <38737226+340757871@users.noreply.github.com>
2018-10-05 12:32:55 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -63,13 +63,21 @@ def display_platform(wrong_guesses):
 
 
 def display_letters(word, guesses):
-    """Displays (prints) the letter-board,
-    underscores (_) for letters not yet guessed.
-    
-    Args:
-        word (str): The secret word
-        guesses (str): A string of correct guesses
-    """
+  word_bank = list(word)
+  guess = list(set(guesses))
+  charac = list(word)
+  lengguess = len(guesses)
+  lengword = len(word)
+  for l in range(0, lengword):
+   if(charac[l] != " "):
+    charac[l] = " _"
+   for a in range(0, lengguess):
+    for l in range(0, word.count(guesses[a])):
+     if(guess[a] in word):
+      und = word_bank.index(guesses[a])
+      word_bank[und] = " _"
+      charac[und] = guesses[a]
+   return " ".join(charac)
     pass
 
 
