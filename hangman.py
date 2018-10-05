1eff801df3a57a9cfc0bdd95fa6d708b7f1049c3
340757871 <38737226+340757871@users.noreply.github.com>
2018-10-05 13:06:13 -0400

    changing the underscore to characters

--- hangman.py
+++ hangman.py
@@ -1,2 +1,22 @@
 # hangman.py 
-    
+"""
+replaces words with the underscores and changes underscores back into characters
+"""
+
+def display_letters(word, guesses):
+    word_bank = list(word)
+    guess = list(set(guesses))
+    charac = list(word)
+    lengguess = len(guesses)
+    lengword = len(word)
+    for l in range(0, lengword):
+        if(charac[l] != " "):
+           charac[l] = " _"
+        for a in range(0, lengguess):
+            for l in range(0, word.count(guesses[a])):
+                if(guess[a] in word):
+                    und = word_bank.index(guesses[a])
+                    word_bank[und] = " _"
+                    charac[und] = guesses[a]
+        return " ".join(charac)
+
