96c3a99224afa84fe276f388c9bdb6c552484d02
Mr. Gallo <daniel.gallo@ycdsbk12.ca>
2018-10-05 14:29:55 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,16 +5,12 @@ def test_get_secret_word():
     assert len(words) >= 5, "Should contain a list of at least 5 words."
     
     # do the following:
-    assert what secret word returns is a string, "Should return a string"
+    # assert what secret word returns is a string, "Should return a string"
 
 
 def test_word_is_solved():
     assert word_is_solved("hello", "eh") == False,"Should not be solved guesses: 'eh' for word: 'hello'."
     assert word_is_solved("world", "dorlw") == True,"Should be solved"
-    assert word_is_solved("paper", "ptaru") == False,"Should not be solved guesses: 'ptaru' for word: 'paper'."
-    assert word_is_solved("table", "aeotbhl") == True,"Should be solved"
-    assert word_is_solved("hello world", "eawldhotr") == True,"Should be solved"
-    assert word_is_solved("hello world", "eawdhotr") == False,"Should not be solved guesses: 'eawl dhotr' for word:'hello world'."
 
     
 def test_display_letters():
