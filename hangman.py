b399d5ad298cb248d155366f25e64d3f6dc068db
YXu <43019609+YXu42@users.noreply.github.com>
2018-10-10 09:46:06 -0400

    Update hangman.py (#117)

--- hangman.py
+++ hangman.py
@@ -183,7 +183,29 @@ def display_letters(word, guesses):
         word (str): The secret word
         guesses (str): A string of correct guesses
     """
-    pass
+    guessed_word = ""
+    counter = 1
+    if guessed_word == "":
+        guessed_word = "_ " * len(word)
+    for i in range(len(guesses)):
+        if word.count(guesses[i]) > 1:
+            index = word.find(guesses[i])
+            for j in range(word.count(guesses[i])):
+                slice1 = guessed_word[ : index * 2]
+                slice2 = guessed_word[index * 2: ]
+                replaced = slice2.replace("_", guesses[i], 1)
+                guessed_word = slice1 + replaced
+                if word.count(guesses[i]) > 1:
+                    replaced = word.replace(guesses[i], "*", counter)
+                    index = replaced.find(guesses[i])
+                counter += 1
+        elif word.count(guesses[i]) == 1:
+            index = word.find(guesses[i])
+            slice1 = guessed_word[ : index * 2]
+            slice2 = guessed_word[index * 2 : ]
+            replaced = slice2.replace("_", guesses[i], 1)
+            guessed_word = slice1 + replaced
+    return guessed_word
 
 
 def get_guess():
